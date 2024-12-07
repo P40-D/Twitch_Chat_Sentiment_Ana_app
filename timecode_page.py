79f316a3f78b91ed779a2e19789a149ff9745e86
P40-D <160519030+P40-D@users.noreply.github.com>
2024-12-07 17:13:14 -0500

    added wordcloud display from timecode

--- timecode_page.py
+++ timecode_page.py
@@ -1,7 +1,9 @@
-
 import streamlit as st
 import pandas as pd
 import plotly.graph_objects as go
+from wordcloud import WordCloud
+import matplotlib.pyplot as plt
+from io import BytesIO
 
 # Load CSV
 df = pd.read_csv('Final_Pestily_Chat.csv')
@@ -26,7 +28,7 @@ def show_timecode_page():
         "**06:27:40** - death of Pestily's in-game character"
     )
     st.write(
-        "*Use the dialog bellow to generate a link to the video at any time you like. Find out what happened that made the comments spike!*"
+        "*Use the dialog below to generate a link to the video at any time you like. Find out what happened that made the comments spike!*"
     )
 
     # Input box for timecode
@@ -36,7 +38,7 @@ def show_timecode_page():
         max_chars=8
     )
 
-    # convert timecode to seconds
+    # Convert timecode to seconds
     def convert_timecode_to_seconds(timecode: str):
         try:
             hours, minutes, seconds = map(int, timecode.split(":"))
@@ -45,15 +47,48 @@ def show_timecode_page():
         except ValueError:
             return None
 
-    # generate Twitch link
+    # word cloud function
+    def generate_wordcloud(timecode):
+        total_seconds = convert_timecode_to_seconds(timecode)
+        if total_seconds is not None:
+            start_time = pd.to_datetime(df.index[0]) + pd.Timedelta(seconds=total_seconds)
+            end_time = start_time + pd.Timedelta(minutes=1)
+            
+            # Filter comments
+            minute_comments = df[(df.index >= start_time) & (df.index < end_time)]
+            text = ' '.join(minute_comments['Message'].dropna())
+            
+            if text:
+                wordcloud = WordCloud(width=800, height=400, background_color="white").generate(text)
+
+                # Convert word cloud to an image
+                fig, ax = plt.subplots(figsize=(8, 4))
+                ax.imshow(wordcloud, interpolation='bilinear')
+                ax.axis('off')
+
+                # BytesIO object
+                buf = BytesIO()
+                plt.savefig(buf, format="png")
+                buf.seek(0)
+
+                # Display
+                st.image(buf)
+
+            else:
+                st.warning("No comments found for this timecode.")
+        else:
+            st.error("Please enter a valid timecode in HH:MM:SS format.")
+            
+    # Generate Twitch link and display word cloud
     if timecode_input:
         total_seconds = convert_timecode_to_seconds(timecode_input)
         if total_seconds is not None:
             twitch_video_url = f"https://www.twitch.tv/videos/2305090004?t={total_seconds}s"
             st.write(f"Here is the link to the Twitch video at {timecode_input}: [Twitch Video](https://www.twitch.tv/videos/2305090004?t={total_seconds}s)")
+
+            generate_wordcloud(timecode_input)
         else:
             st.error("Please enter a valid timecode in HH:MM:SS format.")
-
 
     # Plot Total Comment Count
     total_comment_counts = df.resample('1T').size()
@@ -63,7 +98,7 @@ def show_timecode_page():
                         name='Total Comment Count', 
                         marker=dict(color='purple', opacity=0.6)))
 
-    # layout
+    # Layout
     fig3.update_layout(
         title={
             'text': "Total Comment Count Every Minute",
